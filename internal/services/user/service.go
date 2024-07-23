8b947974b8c4ef0cb39d4412b60f609dbf5b2b41
DaemonDxx <i79535999238@gmail.com>
2024-07-23 21:29:11 +0300

    [feature] refactor service errors

--- internal/services/user/service.go
+++ internal/services/user/service.go
@@ -6,10 +6,9 @@ import (
 	"fmt"
 	"github.com/daemondxx/lks_back/entity"
 	"github.com/daemondxx/lks_back/internal/services"
-	"gorm.io/gorm"
 )
 
-const serName = "userService"
+const servName = "userService"
 
 var (
 	ErrUserIsRegister = errors.New("user is register in system")
@@ -42,7 +41,7 @@ func (s *Service) Register(ctx context.Context, accLogin string, accPass string,
 
 	if err != nil {
 		return user, &services.ErrServ{
-			Service: serName,
+			Service: servName,
 			Message: "find user with accord login error",
 			Err:     err,
 		}
@@ -60,7 +59,7 @@ func (s *Service) Register(ctx context.Context, accLogin string, accPass string,
 
 	if err := s.d.Create(ctx, &user); err != nil {
 		return user, &services.ErrServ{
-			Service: serName,
+			Service: servName,
 			Message: "create user error",
 			Err:     err,
 		}
@@ -70,13 +69,17 @@ func (s *Service) Register(ctx context.Context, accLogin string, accPass string,
 }
 
 func (s *Service) UpdateAccord(ctx context.Context, userID uint, login string, password string) error {
+	if err := s.hasUserByID(ctx, userID); err != nil {
+		return err
+	}
+
 	if err := s.d.Update(ctx, entity.User{
 		ID:             userID,
 		AccordLogin:    login,
 		AccordPassword: password,
 	}); err != nil {
 		return &services.ErrServ{
-			Service: serName,
+			Service: servName,
 			Message: "update accord auth info error",
 			Err:     err,
 		}
@@ -85,33 +88,88 @@ func (s *Service) UpdateAccord(ctx context.Context, userID uint, login string, p
 }
 
 func (s *Service) UpdateLKS(ctx context.Context, userID uint, login string, password string) error {
+	if err := s.hasUserByID(ctx, userID); err != nil {
+		return err
+	}
+
 	if err := s.d.Update(ctx, entity.User{
 		ID:             userID,
 		AccordLogin:    login,
 		AccordPassword: password,
 	}); err != nil {
-		return fmt.Errorf("update accord auth info error: %e", err)
+		return &services.ErrServ{
+			Service: servName,
+			Message: "update lks info error",
+			Err:     err,
+		}
 	}
 	return nil
 }
 
 func (s *Service) UpdateActiveStatus(ctx context.Context, userID uint, status bool) error {
+	if err := s.hasUserByID(ctx, userID); err != nil {
+		return err
+	}
+
 	if err := s.d.Update(ctx, entity.User{
 		ID:       userID,
 		IsActive: status,
 	}); err != nil {
-		return fmt.Errorf("update user status error: %w", err)
+		return &services.ErrServ{
+			Service: servName,
+			Message: "update user status error",
+			Err:     err,
+		}
 	}
 	return nil
 }
 
 func (s *Service) GetUserByAccordLogin(ctx context.Context, accLogin string) (entity.User, error) {
-	user, err := s.d.Find(ctx, &entity.User{AccordLogin: accLogin})
+	var u entity.User
+	users, err := s.d.Find(ctx, &entity.User{AccordLogin: accLogin})
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return entity.User{}, ErrUserNotFound
+		return u, &services.ErrServ{
+			Service: servName,
+			Message: "find user by accord login error",
+			Err:     err,
 		}
-		return entity.User{}, fmt.Errorf("find user by id error: %e", err)
 	}
-	return user[0], nil
+
+	if len(users) == 0 {
+		return u, ErrUserNotFound
+	}
+
+	u = users[0]
+	return u, nil
+}
+
+func (s *Service) GetUserByID(ctx context.Context, id uint) (entity.User, error) {
+	if u, err := s.d.GetByID(ctx, id); err != nil {
+		return u, &services.ErrServ{
+			Service: servName,
+			Message: "get user by id error",
+			Err:     err,
+		}
+	} else {
+		return u, nil
+	}
+}
+
+func (s *Service) hasUserByID(ctx context.Context, userID uint) error {
+	if _, err := s.d.GetByID(ctx, userID); err != nil {
+		if errors.Is(err, ErrUserNotFound) {
+			return &services.ErrServ{
+				Service: servName,
+				Message: fmt.Sprintf("user with id %d not found", userID),
+				Err:     err,
+			}
+		} else {
+			return &services.ErrServ{
+				Service: servName,
+				Message: "find user by id error",
+				Err:     err,
+			}
+		}
+	}
+	return nil
 }
