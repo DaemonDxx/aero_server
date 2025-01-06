5651f89386923f50b21acf918beb638c6311ebec
DaemonDxx <i79535999238@gmail.com>
2025-01-06 16:36:09 +0300

    [refactor] add service name param to new func logged service

--- internal/services/loggerservice.go
+++ internal/services/loggerservice.go
@@ -1,14 +1,27 @@
 package services
 
-import "github.com/rs/zerolog"
+import (
+	"github.com/rs/zerolog"
+	"os"
+)
 
 type LoggedService struct {
 	log *zerolog.Logger
 }
 
-func NewLoggedService(log *zerolog.Logger) LoggedService {
+func NewLoggedService(servName string, log *zerolog.Logger) LoggedService {
+	var l zerolog.Logger
+
+	if log == nil {
+		var l zerolog.Logger
+		l = zerolog.New(os.Stdout).Level(zerolog.NoLevel)
+		log = &l
+	} else {
+		l = log.With().Str("service", servName).Logger()
+	}
+
 	return LoggedService{
-		log: log,
+		log: &l,
 	}
 }
 
