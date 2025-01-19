bdb0c5547e6a30b4b373236939cccf5d43a8c289
DaemonDxx <i79535999238@gmail.com>
2025-01-19 15:48:00 +0300

    [feat] add order controller with get method

---
+++ internal/server/controllers/order/controller.go
@@ -0,0 +1,24 @@
+package order
+
+import (
+	"context"
+	"github.com/daemondxx/lks_back/entity"
+	"github.com/gin-gonic/gin"
+)
+
+type OrderService interface {
+	GetActualOrders(ctx context.Context, acc entity.Account) ([]entity.Order, error)
+	ConfirmOrder(ctx context.Context, acc entity.Account, orderID uint) error
+}
+
+type controller struct {
+	serv OrderService
+}
+
+func NewOrderController(serv OrderService) *controller {
+	return &controller{}
+}
+
+func (ctrl *controller) ApplyHandlers(g *gin.RouterGroup) {
+	g.GET("/", ctrl.Get)
+}
