b2e17e4dcdb9774825553e102b79b30fc354566a
DaemonDxx <i79535999238@gmail.com>
2025-01-26 20:35:44 +0300

    [feat] add test

---
+++ internal/server/services/collector/diff_test.go
@@ -0,0 +1,192 @@
+package collector
+
+import (
+	"github.com/daemondxx/lks_back/entity"
+	"github.com/daemondxx/lks_back/internal/logger"
+	"github.com/daemondxx/lks_back/internal/services"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
+	"testing"
+	"time"
+)
+
+var defaultTime = time.Unix(1737786876, 0)
+
+var suites = []struct {
+	name          string
+	pulledItems   []entity.OrderItem
+	actualItems   []entity.OrderItem
+	expectedID    []uint
+	isHasErrEmpty bool
+}{
+	{
+		name: "Empty actual, some pulled",
+		pulledItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 10,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 20,
+				},
+				Departure: getTimeWithOffset(7),
+			},
+		},
+		actualItems:   []entity.OrderItem{},
+		expectedID:    []uint{10, 20},
+		isHasErrEmpty: false,
+	},
+	{
+		name: "have actual, has not new pulled",
+		pulledItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 10,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 20,
+				},
+				Departure: getTimeWithOffset(7),
+			},
+		},
+		actualItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 2,
+				},
+				Departure: getTimeWithOffset(7),
+			},
+		},
+		expectedID:    []uint{},
+		isHasErrEmpty: true,
+	},
+	{
+		name: "have actual, has new pulled",
+		pulledItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 20,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 30,
+				},
+				Departure: getTimeWithOffset(6),
+			},
+			{
+				Model: gorm.Model{
+					ID: 40,
+				},
+				Departure: getTimeWithOffset(7),
+			},
+		},
+		actualItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 2,
+				},
+				Departure: getTimeWithOffset(6),
+			},
+		},
+		expectedID:    []uint{40},
+		isHasErrEmpty: false,
+	},
+	{
+		name: "have actual, has new pulled without crossing",
+		pulledItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 20,
+				},
+				Departure: getTimeWithOffset(5),
+			},
+			{
+				Model: gorm.Model{
+					ID: 30,
+				},
+				Departure: getTimeWithOffset(6),
+			},
+		},
+		actualItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Departure: getTimeWithOffset(3),
+			},
+			{
+				Model: gorm.Model{
+					ID: 2,
+				},
+				Departure: getTimeWithOffset(4),
+			},
+		},
+		expectedID:    []uint{20, 30},
+		isHasErrEmpty: false,
+	},
+	{
+		name:        "have actual, empty pulled",
+		pulledItems: []entity.OrderItem{},
+		actualItems: []entity.OrderItem{
+			{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Departure: getTimeWithOffset(3),
+			},
+		},
+		expectedID:    []uint{},
+		isHasErrEmpty: true,
+	},
+}
+
+func TestDiffMethod(t *testing.T) {
+	serv := Service{
+		LoggedService: services.NewLoggedService("diff_test", logger.NewLogger("DEV")),
+	}
+	for _, s := range suites {
+		t.Run(s.name, func(t *testing.T) {
+			items, err := serv.diff(s.pulledItems, s.actualItems)
+			if s.isHasErrEmpty {
+				require.ErrorIs(t, err, errHasNotNewItems)
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, len(s.expectedID), len(items))
+				assert.Equal(t, s.expectedID, extractID(items))
+			}
+		})
+	}
+}
+
+func extractID(i []entity.OrderItem) []uint {
+	r := make([]uint, 0, len(i))
+	for _, v := range i {
+		r = append(r, v.ID)
+	}
+	return r
+}
+
+func getTimeWithOffset(o int) time.Time {
+	return defaultTime.Add(time.Duration(o) * time.Hour)
+}
---
+++ internal/server/services/collector/pull_test.go
@@ -0,0 +1,182 @@
+package collector
+
+import (
+	"context"
+	"github.com/daemondxx/lks_back/entity"
+	"github.com/daemondxx/lks_back/internal/logger"
+	"github.com/daemondxx/lks_back/internal/services"
+	collector_mock "github.com/daemondxx/lks_back/mocks/server/services/collector"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
+	"testing"
+)
+
+type collectorPullSuite struct {
+	suite.Suite
+	dao  *collector_mock.MockOrderDAO
+	api  *collector_mock.MockLKSApi
+	cr   *entity.Credential
+	serv *Service
+}
+
+func TestCollectorPullSuite(t *testing.T) {
+	suite.Run(t, new(collectorPullSuite))
+}
+
+func (s *collectorPullSuite) SetupTest() {
+	s.dao = &collector_mock.MockOrderDAO{}
+	s.api = &collector_mock.MockLKSApi{}
+	s.cr = &entity.Credential{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		AccordLogin:    "Test",
+		LKSLogin:       "Test",
+		AccordPassword: "Test",
+		LKSPassword:    "Test",
+		IsActual:       true,
+	}
+	s.serv = &Service{
+		LoggedService: services.NewLoggedService("collector_test", logger.NewLogger("DEV")),
+		dao:           s.dao,
+		api:           s.api,
+	}
+}
+
+func (s *collectorPullSuite) TestSuccessPullFirstOrder() {
+	pi := []entity.OrderItem{
+		{
+			Departure: getTimeWithOffset(5),
+		},
+	}
+	findFn := s.dao.EXPECT().FindLastOrders(mock.Anything, s.cr, 1).Return(make([]entity.Order, 0), nil)
+	defer findFn.Unset()
+	apiFn := s.api.EXPECT().GetActualDuty(mock.Anything, s.cr).Return(pi, nil)
+	defer apiFn.Unset()
+	saveFn := s.dao.EXPECT().Save(mock.Anything, mock.Anything).Return(nil)
+	defer saveFn.Unset()
+
+	o, err := s.serv.Pull(context.Background(), s.cr)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), o)
+
+	assert.Equal(s.T(), len(pi), len(o.Items))
+	assert.Equal(s.T(), s.cr.ID, o.CredentialID)
+}
+
+func (s *collectorPullSuite) TestPullOldOrder() {
+	pi := []entity.OrderItem{
+		{
+			Departure: getTimeWithOffset(5),
+		},
+	}
+	o := entity.Order{
+		Model:        gorm.Model{},
+		CredentialID: s.cr.ID,
+		Items: []entity.OrderItem{
+			{
+				Departure: getTimeWithOffset(5),
+			},
+		},
+		Status: 0,
+	}
+	findFn := s.dao.EXPECT().FindLastOrders(mock.Anything, s.cr, 1).Return([]entity.Order{o}, nil)
+	defer findFn.Unset()
+	apiFn := s.api.EXPECT().GetActualDuty(mock.Anything, s.cr).Return(pi, nil)
+	defer apiFn.Unset()
+
+	or, err := s.serv.Pull(context.Background(), s.cr)
+	require.ErrorIs(s.T(), err, ErrOrderIsExist)
+	require.Nil(s.T(), or)
+}
+
+func (s *collectorPullSuite) TestPullNewOrderWithEmptyLast() {
+	pi := []entity.OrderItem{
+		{
+			Departure: getTimeWithOffset(5),
+		},
+	}
+	o := entity.Order{
+		Model:        gorm.Model{},
+		CredentialID: s.cr.ID,
+		Items:        []entity.OrderItem{},
+		Status:       0,
+	}
+	findFn := s.dao.EXPECT().FindLastOrders(mock.Anything, s.cr, 1).Return([]entity.Order{o}, nil)
+	defer findFn.Unset()
+	apiFn := s.api.EXPECT().GetActualDuty(mock.Anything, s.cr).Return(pi, nil)
+	defer apiFn.Unset()
+	saveFn := s.dao.EXPECT().Save(mock.Anything, mock.Anything).Return(nil)
+	defer saveFn.Unset()
+
+	or, err := s.serv.Pull(context.Background(), s.cr)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), or)
+
+	assert.Equal(s.T(), len(pi), len(or.Items))
+	assert.Equal(s.T(), s.cr.ID, or.CredentialID)
+}
+
+func (s *collectorPullSuite) TestPullNewOrderWithLast() {
+	pi := []entity.OrderItem{
+		{
+			Departure: getTimeWithOffset(5),
+		},
+		{
+			Departure: getTimeWithOffset(7),
+		},
+	}
+	o := entity.Order{
+		Model:        gorm.Model{},
+		CredentialID: s.cr.ID,
+		Items: []entity.OrderItem{
+			{
+				Departure: getTimeWithOffset(5),
+			},
+		},
+		Status: 0,
+	}
+	findFn := s.dao.EXPECT().FindLastOrders(mock.Anything, s.cr, 1).Return([]entity.Order{o}, nil)
+	defer findFn.Unset()
+	apiFn := s.api.EXPECT().GetActualDuty(mock.Anything, s.cr).Return(pi, nil)
+	defer apiFn.Unset()
+	saveFn := s.dao.EXPECT().Save(mock.Anything, mock.Anything).Return(nil)
+	defer saveFn.Unset()
+
+	or, err := s.serv.Pull(context.Background(), s.cr)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), or)
+
+	assert.Equal(s.T(), 1, len(or.Items))
+	assert.Equal(s.T(), s.cr.ID, or.CredentialID)
+}
+
+func (s *collectorPullSuite) TestPullEmptyOrder() {
+	var pi []entity.OrderItem
+	o := entity.Order{
+		Model:        gorm.Model{},
+		CredentialID: s.cr.ID,
+		Items: []entity.OrderItem{
+			{
+				Departure: getTimeWithOffset(5),
+			},
+		},
+		Status: 0,
+	}
+	findFn := s.dao.EXPECT().FindLastOrders(mock.Anything, s.cr, 1).Return([]entity.Order{o}, nil)
+	defer findFn.Unset()
+	apiFn := s.api.EXPECT().GetActualDuty(mock.Anything, s.cr).Return(pi, nil)
+	defer apiFn.Unset()
+	saveFn := s.dao.EXPECT().Save(mock.Anything, mock.Anything).Return(nil)
+	defer saveFn.Unset()
+
+	or, err := s.serv.Pull(context.Background(), s.cr)
+	require.NoError(s.T(), err)
+	require.NotNil(s.T(), or)
+
+	assert.Equal(s.T(), 0, len(or.Items))
+	assert.Equal(s.T(), s.cr.ID, or.CredentialID)
+}
